9943600bc4d2effc7e58d644b5c8ee6b8fbcfcf5
admin <admin@DESKTOP-34FMDDF>
2021-04-04 09:57:18 +0530

    hi all this is our new project on kite zerodha

---
+++ src/sample/Demo.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class Demo {
+                  public static void main(String[] args) {
+					System.out.println("hi all how are you?");
+					System.out.println("hope you all are fine there");
+				}
+}
